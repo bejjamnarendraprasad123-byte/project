a631f6ab9b92e60f3d27430781a1c4598da72491
rajasrigajengi8 <rajasrigajengi00@gmail.com>
2026-05-28 13:23:10 +0530

    Add files via upload

---
+++ Simple-Calculator/simple_calculator.py
@@ -0,0 +1,40 @@
+while True:
+    print("\n ----Simple Calculator----")
+    print("1.Addition")
+    print("2.Subtraction")
+    print("3.Multiplication")
+    print("4.Division")
+    print("5.Exit")
+
+    choice = input("Enter your choice(1-5): ")
+
+    if choice == '5':
+        print("Calculator closed.")
+        break
+
+    if choice in ['1', '2', '3', '4']:
+
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            result = num1 + num2
+            print("Result:", result)
+
+        elif choice == '2':
+            result = num1 - num2
+            print("Result:", result)
+
+        elif choice == '3':
+            result = num1 * num2
+            print("Result:", result)
+
+        elif choice == '4':
+            if num2 != 0:
+                result = num1 / num2
+                print("Result:", result)
+            else:
+                print("Division by zero is not allowed.")
+
+    else:
+        print("Invalid choice. Please select between 1 and 5.")
